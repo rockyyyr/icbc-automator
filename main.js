6c1de2d4d9605d60bd481c7765eb2175d62cffe4
rockyyyr <rockyrobson@hotmail.com>
2024-10-06 17:36:36 -0700

    added log

--- main.js
+++ main.js
@@ -33,6 +33,8 @@ async function run() {
                 foundResults = true;
 
                 for (const result of results) {
+                    console.log(result);
+
                     await tg.sendMessage(foundAppointmentMessage(location.name, result.time.format(Time.displayFormat)));
                 }
             }
